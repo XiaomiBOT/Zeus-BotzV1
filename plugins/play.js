fb2000674b4f397a82892162ba1501809e7b576a
PilarV2 <97302454+PilarV2@users.noreply.github.com>
2022-02-03 11:04:44 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -31,7 +31,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 *Ukuran File Audio:* ${filesizeF}
 *Ukuran File Video:* ${yt2.filesizeF}
 *Server y2mate:* ${usedServer}
-`.trim(), 'JANGAN LUPA JUGA SUB YT OWNER DENGAN KETIK /ytowner', 'Audio', `.yta ${vid.url}`, 'Video', `.yt ${vid.url}`)
+`.trim(), 'ZEUS BOTZ', 'Audio', `.yta ${vid.url}`, 'Video', `.yt ${vid.url}`)
 }
 handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
